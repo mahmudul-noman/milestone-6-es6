a6c1fbbdab474ef439b063f93846fad1717c7c95
Mahmudul-H-Noman <mhnoman75@gmail.com>
2023-02-21 18:55:52 +0600

    module 31 COMPLETE

---
+++ quiz.js
@@ -0,0 +1,48 @@
+// const numbers = [45, 12, 36, 84];
+// numbers.unshift(22);
+// console.log(numbers);
+
+
+
+// function getSum(a, b=9){
+//     return a+b;
+// } 
+// console.log(getSum(2, 7));
+
+
+// const y = x => x*x;
+// const z = y(5);
+// console.log(z);
+
+
+// const name = 'Hero'; 
+// // const greetings = 'Welcome Home!'+' '+name;
+// const greetings = `Welcome Home! ${name}`;
+// console.log(greetings);
+
+
+// const myFunc = (name) => console.log('Hero!');
+
+
+
+// const division = num1, num2 => console.log(num1 / num2);
+// const value = division(4, 2);
+// // console.log(value);
+
+
+
+// const nayikas = ['mousumi', 'popy', 'shabnur', 'purnima'];
+// const array2 = [...nayikas];
+// console.log(array2);
+// console.log(nayikas);
+
+
+
+// const numbers = [10, 20, 30, 40, 50, 60, 70];
+// const largest = Math.max[...numbers];
+// console.log(largest);
+
+
+const cars = ['toyota', 'honda', 'mercedes']; 
+const newCars = [...cars, 'ferrari'];
+console.log(newCars);
---
+++ spread.js
@@ -0,0 +1,6 @@
+const max = Math.max(5, 8, 7, 9, 2);
+// console.log(max);
+
+const numbers = [2, 12, 45, 8, 79];
+const largest = Math.max(...numbers);
+console.log(largest);
